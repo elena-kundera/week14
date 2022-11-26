b87224ea07f5201a5aab816a47a163e2778492c6
Елена <elena@MacBook-Air-Elena-2.local>
2022-11-26 17:22:13 +0300

    Добавила обновленный калькулятор

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// 1. Перепишите домашнее задание прошлой недели c помощью **input** для ввода данных 
+// и **div** (либо **input**) для вывода результата
+
+// 2. Напишите скрипт, который меняет фон div-ов по клику на них
+
+// 3. Напишите простую галерею на 2+ картинки с кнопками Вперед и Назад 
+// (*она не должна работать идеально, это пока просто заготовка на будущее!*)
+
+
+// 4. Воспользовавшись дебаггером, пройдите по шагам все практическое домашнее задание. 
+// Если что-то не работает, попытайтесь понять, почему
+
+
+const data = document.querySelector('.data');
+const inputPage = document.getElementById("inputHi");
+const btnPage = document.getElementById("btnHi");
+
+
+function getInputValue() {
+
+    console.log(inputPage.value);
+    const userName = document.getElementById("inputHi").value;
+    data.innerHTML = "Привет, " + userName + ", приятно познакомиться!"
+    
+
+}
+
+
+const data2 = document.querySelector('.calcData');
+const inputOne = document.getElementById("calcInput1");
+const operation = document.getElementById("calc");
+const inputTwo = document.getElementById("calcInput2");
+const countBtn = document.getElementById("calcBtn");
+
+
+function getCalcValuePlus() {
+    const num1 = Number(document.getElementById("calcInput1").value);
+    const num2 = Number(document.getElementById("calcInput2").value);
+    const result = num1 + num2;
+    data2.innerHTML = "Ответ: " + result;
+}
+
+function getCalcValueMinus() {
+    const num1 = Number(document.getElementById("calcInput1").value);
+    const num2 = Number(document.getElementById("calcInput2").value);
+    const result = num1 - num2;
+    data2.innerHTML = "Ответ: " + result;
+}
+
+function getCalcValueMult() {
+    const num1 = Number(document.getElementById("calcInput1").value);
+    const num2 = Number(document.getElementById("calcInput2").value);
+    const result = num1 * num2;
+    data2.innerHTML = "Ответ: " + result;
+}
+
+function getCalcValueDivis() {
+    const num1 = Number(document.getElementById("calcInput1").value);
+    const num2 = Number(document.getElementById("calcInput2").value);
+    const result = num1 / num2;
+    data2.innerHTML = "Ответ: " + result;
+}
+
+
+
